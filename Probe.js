8f2665d831a21c68aa39e2f9426c636dd1ce8738
ok <oliwersukic@gmail.com>
2017-03-18 16:50:31 +0100

    Initial commit

---
+++ Probe.js
@@ -0,0 +1,12 @@
+
+
+<scriptsrc="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js">
+$(document).ready(function(){
+	$("#Bilder").hide();
+	
+});
+$("button").click(function(){
+     $("#Bilder").hide();
+ });
+
+</script>
